a858e3886f45ae979dc93a173b720d92a595ba30
benfrank241 <benjamenba@yahoo.ca>
2023-02-02 16:11:16 -0600

    test

--- backend/app.py
+++ backend/app.py
@@ -6,18 +6,22 @@ app = Flask(__name__)
 CORS(app)
 
 # Connect to the MySQL database
-cnx = mysql.connector.connect(user='root', password='root2023',
-                              host='34.29.90.189', database='baseball')
+# cnx = mysql.connector.connect(user='root', password='root2023',
+#                               host='34.29.90.189', database='baseball')
 
-@app.route("/data", methods=["GET"])
-def get_data():
-    cursor = cnx.cursor()
-    query = "SHOW COLUMNS FROM baseball.offense"
-    cursor.execute(query)
-    data = cursor.fetchall()
-    cursor.close()
-    print(jsonify(data))
-    return jsonify(data)
+# @app.route("/data", methods=["GET"])
+# def get_data():
+#     cursor = cnx.cursor()
+#     query = "SELECT * from baseball.offense"
+#     cursor.execute(query)
+#     data = cursor.fetchall()
+#     cursor.close()
+#     print(jsonify(data))
+#     return jsonify(data)
+
+@app.route("/hello")
+def hello():
+    return "Hello World"
 
 if __name__ == "__main__":
     app.run(debug=True)
