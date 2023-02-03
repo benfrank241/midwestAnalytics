a2904fa09ca0f0db250a5f048e50161a9299dfe4
benfrank241 <benjamenba@yahoo.ca>
2023-02-03 13:09:43 -0600

    test using official docs

--- backend/app.py
+++ backend/app.py
@@ -1,6 +1,9 @@
 from flask import Flask, jsonify, request
-import mysql.connector
 from flask_cors import CORS
+import os
+from google.cloud.sql.connector import Connector, IPTypes
+import pymysql
+import sqlalchemy
 # from google.oauth2.credentials import Credentials
 
 # creds = Credentials.from_authorized_user_file("C:/Users/benja/AppData/Roaming/gcloud/application_default_credentials.json")
@@ -8,21 +11,57 @@ from flask_cors import CORS
 
 app = Flask(__name__)
 CORS(app)
-
-# Connect to the MySQL database
-cnx = mysql.connector.connect(user='root', password='root2023',
-                              host='34.29.90.189', database='baseball')
-
 @app.route("/data", methods=["GET"])
-def get_data():
+def connect_with_connector() -> sqlalchemy.engine.base.Engine:
+    """
+    Initializes a connection pool for a Cloud SQL instance of MySQL.
 
-    cursor = cnx.cursor()
-    query = "SELECT * from baseball.offense"
-    cursor.execute(query)
-    data = cursor.fetchall()
-    cursor.close()
-    print(jsonify(data))
-    return jsonify(data)
+    Uses the Cloud SQL Python Connector package.
+    """
+    # Note: Saving credentials in environment variables is convenient, but not
+    # secure - consider a more secure solution such as
+    # Cloud Secret Manager (https://cloud.google.com/secret-manager) to help
+    # keep secrets safe.
+
+    instance_connection_name = os.environ["sonic-solstice-376503:us-central1:analytics"]  # e.g. 'project:region:instance'
+    db_user = os.environ.get("benbart02", "")  # e.g. 'my-db-user'
+    db_pass = os.environ["root2023"]  # e.g. 'my-db-password'
+    db_name = os.environ["baseball"]  # e.g. 'my-database'
+
+    ip_type = IPTypes.PRIVATE if os.environ.get("PRIVATE_IP") else IPTypes.PUBLIC
+
+    connector = Connector(ip_type)
+
+    def getconn() -> pymysql.connections.Connection:
+        conn: pymysql.connections.Connection = connector.connect(
+            instance_connection_name,
+            "pymysql",
+            user=db_user,
+            password=db_pass,
+            db=db_name,
+        )
+        return conn
+
+    pool = sqlalchemy.create_engine(
+        "mysql+pymysql://",
+        creator=getconn,
+    )
+
+    print("success")
+
+    return pool
+
+
+
+
+# def get_data():
+#     with conn() as cursor:
+#         query = "SELECT * from baseball.offense"
+#         cursor.execute(query)
+#         data = cursor.fetchall()
+#         cursor.close()
+#         print(jsonify(data))
+#         return jsonify(data)
 
 @app.route("/hello")
 def hello():
