9ab7748b394855a874bf6da58ae1e6a58d71c196
benfrank241 <benjamenba@yahoo.ca>
2023-02-04 21:55:09 -0600

    Update app.py

--- backend/app.py
+++ backend/app.py
@@ -5,7 +5,7 @@ import mysql.connector
 
 app = Flask(__name__)
 CORS(app)
-@app.route("/data", methods=["GET"])
+@app.route("/", methods=["GET"])
 def conn():
     cnx = mysql.connector.connect( host="34.29.90.189", user="ben", password="root2023", database="baseball" )
 
@@ -22,3 +22,6 @@ def hello():
 
 if __name__ == "__main__":
     app.run(debug=True)
+
+#TODO: add app ip to accepted connections
+
