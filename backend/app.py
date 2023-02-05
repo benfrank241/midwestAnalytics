6098c6f28c3c5f5a6b88aac6ab1dd148cea60b0d
benfrank241 <benjamenba@yahoo.ca>
2023-02-05 13:51:55 -0600

    Added security

--- backend/app.py
+++ backend/app.py
@@ -2,12 +2,16 @@ from flask import Flask, jsonify, request
 from flask_cors import CORS
 import ssl
 import mysql.connector
+import os
+
+p = os.environ.get('CLOUD_SQL_PASSWORD')
+# print(p)
 
 app = Flask(__name__)
 CORS(app)
 @app.route("/", methods=["GET"])
 def conn():
-    cnx = mysql.connector.connect( host="34.29.90.189", user="ben", password="root2023", database="baseball" )
+    cnx = mysql.connector.connect( host="34.29.90.189", user="ben", password=p, database="baseball" )
 
     cursor = cnx.cursor()
     cursor.execute("SELECT * FROM baseball.offense")
